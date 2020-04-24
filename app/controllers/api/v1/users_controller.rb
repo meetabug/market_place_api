6e3966aabf8787abee0f781aebd1dfb47ab91400
meetabug <1272526724@qq.com>
2020-04-24 21:58:35 +0800

    Restrict actions for unauthorized users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
+  before_action :check_owner, only: %i[update destroy]
 
   # GET /users/1
   def show
@@ -36,6 +37,10 @@ class Api::V1::UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def check_owner
+    head :forbidden unless @user.id == current_user&.id
   end
 
   # Only allow a trusted parameter "white list" through.
